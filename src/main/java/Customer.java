b22d61e297b97240f83864e82cd5d6ddd22e3228
Rayster Ramos <rjrfrst@gmail.com>
2023-10-15 17:08:06 +0100

    This is the third commit, added customer and customerTest files.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,64 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Customer {
-}
+
+    //instance fields | properties
+    private String name;
+    private int wallet;
+
+    //This Array list has the value of 0 therefore not needed to add in the constructor.
+    private ArrayList<Artwork> artCollection;
+
+    //Constructors
+    public Customer(String inputName, int inputWallet){
+        this.name = inputName;
+        this.wallet = inputWallet;
+        this.artCollection = new ArrayList<>();
+    }
+
+    //Methods
+    //Look at how we use the artwork.getPrice() from the Artwork class
+    public boolean canBuy(Artwork artwork){
+        if (artwork.getPrice() <= this.wallet){
+            return true;
+        }
+        return false;
+    }
+
+    public void buyArtwork(Artwork artwork){
+        this.wallet -= artwork.getPrice();
+        this.artCollection.add(artwork);
+    }
+    public int getArtworkCount(){
+        return this.artCollection.size();
+    }
+
+
+    //Getters and Setters
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getWallet(){
+        return  this.wallet;
+    }
+
+    public void setWallet(int wallet){
+        this.wallet = wallet;
+    }
+
+    public ArrayList<Artwork> getCustomerArtwork(){
+        return artCollection;
+    }
+
+    public void setCustomerArtwork(ArrayList<Artwork> customerArtwork){
+        this.artCollection = customerArtwork;
+    }
+
+
+} // last curly bracket
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,66 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class CustomerTest {
-}
+
+    Customer customer;
+    Artist artist1;
+    Artwork artwork1;
+    Artist artist2;
+    Artwork artwork2;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer("Mike", 1000);
+
+        artist1 = new Artist("John");
+        artwork1 = new Artwork("Glass", artist1, 100);
+
+        artist2 = new Artist("James");
+        artwork2 = new Artwork("Cup", artist2, 2000);
+    }
+
+    //Testing methods
+    //Look at the first two test, we wrote __true || __false, this convention makes it easier to read code.
+    @Test
+    public void canAffordArtwork__true(){
+        //When && Then
+        assertThat(customer.canBuy(artwork1)).isEqualTo(true);
+
+    }
+
+    @Test
+    public void canAffordArtwork__false(){
+        //When && Then
+        assertThat(customer.canBuy(artwork2)).isEqualTo(false);
+    }
+
+    @Test
+    public void customerArtworkCountStartsNothing(){
+        assertThat(customer.getArtworkCount()).isEqualTo(0);
+    }
+
+    @Test
+    public void canBuyArtwork(){
+        customer.buyArtwork(artwork1);
+        assertThat(customer.getWallet()).isEqualTo(900);
+        assertThat(customer.getArtworkCount()).isEqualTo(1);
+    }
+
+    //Testing Getters and Setters
+
+    @Test
+    public void hasName(){
+        assertThat(customer.getName()).isEqualTo("Mike");
+    }
+
+    @Test
+    public  void hasWallet(){
+        assertThat(customer.getWallet()).isEqualTo(1000);
+    }
+
+
+
+} // last curly bracket
